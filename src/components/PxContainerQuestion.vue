f3e0f711a4f2dd33db0fb727633b42e51ac54a45
LuiDev2001 <luisqrja@gmail.com>
2021-07-23 20:34:31 -0500

    styles: put responsive desing

--- src/components/PxContainerQuestion.vue
+++ src/components/PxContainerQuestion.vue
@@ -14,7 +14,7 @@ export default {
 @import "~@/assets/sass/utils/_animation.scss";
 .card-quiz {
   background: #ffffff;
-  padding: 2.9rem 2rem 4.25rem;
+  padding: 2rem 1.5rem 1.5rem;
   border-radius: 1.5rem;
   height: auto;
   position: relative;
@@ -23,5 +23,8 @@ export default {
   animation-name: fadeIn;
   animation-duration: 0.7s;
   animation-timing-function: ease-in;
+  @media screen and (min-width: 768px) {
+    padding: 2.9rem 2rem 4.25rem;
+  }
 }
 </style>
